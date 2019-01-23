071e18b532740c08a7283dde4cbb8d7556c83715
Kalle <qalle85@gmail.com>
2019-01-23 15:07:13 +0200

    merge small files here

--- quantum.asm
+++ quantum.asm
@@ -1,5 +1,40 @@
-    ; Quantum Disco Brothers disassembly
+; Quantum Disco Brothers disassembly
 
-    .include "header.asm"
-    .incbin "prg.bin"
-    .include "chr.asm"
+    .include "macros.asm"
+    .include "constants.asm"
+
+; -----------------------------------------------------------------------------
+
+    ; iNES header
+
+    .org $0000
+
+    .byte "NES", $1a            ; identifier
+    .byte 2                     ; 2*16 KiB PRG-ROM
+    .byte 4                     ; 4*8 KiB CHR-ROM
+    .byte %00110000, %00000000  ; mapper 3 (CNROM), horizontal mirroring
+
+    .advance $0010, $00
+
+; -----------------------------------------------------------------------------
+
+    ; PRG ROM
+    ; 32 KiB (technically one bank but the program aligns nicely with the
+    ; 16-KiB boundary)
+
+    .org $8000
+    .include "prg0.asm"   ; first half
+    .advance $c000, $00
+    .include "prg1.asm"   ; second half minus the interrupt vectors
+    .advance $fffa, $00
+    .word nmi, init, irq  ; interrupt vectors
+    .advance $10000, $00
+
+; -----------------------------------------------------------------------------
+
+    ; CHR ROM
+    ; 32 KiB (4*8 KiB)
+
+    .org $0000
+    .incbin "chr.bin"
+    .advance $8000, $00

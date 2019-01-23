9ebc67936974ea4758bd0f8d1a9eac1968d34a05
Kalle <qalle85@gmail.com>
2019-01-23 15:06:38 +0200

    everything in one go

--- assemble.bat
+++ assemble.bat
@@ -1,22 +1,6 @@
 @echo off
 cls
 
-echo *** Assembling PRG ROM ***
-ophis -v -o prg.bin prg.asm
-if errorlevel 1 goto end
-if not exist prg.bin goto end
-echo.
-
-echo *** Verifying PRG ROM ***
-if exist prg-original.bin goto verifyprg
-echo Warning: "prg-original.bin not found", cannot verify.
-echo.
-goto assembleall
-:verifyprg
-fc /b prg-original.bin prg.bin
-if errorlevel 1 goto end
-
-:assembleall
 echo *** Assembling iNES file ***
 ophis -v -o quantum.nes quantum.asm
 if errorlevel 1 goto end
@@ -24,11 +8,6 @@ if not exist quantum.nes goto end
 echo.
 
 echo *** Verifying iNES file ***
-if exist quantum-original.nes goto verifyall
-echo Warning: "quantum-original.nes" not found, cannot verify.
-goto end
-:verifyall
 fc /b quantum-original.nes quantum.nes
-if errorlevel 1 goto end
 
 :end

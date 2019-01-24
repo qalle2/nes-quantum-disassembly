b0da999ad6da23fbc64dc478422ac4b0947cdc5a
Kalle <qalle85@gmail.com>
2019-01-24 22:42:15 +0200

    PPU macros

--- macros.asm
+++ macros.asm
@@ -15,7 +15,7 @@
         lda #_1
         sta ^-1
     .macend
-    
+
     .macro inx4
         inx
         inx
@@ -37,8 +37,46 @@
         lsr
     .macend
 
+    .macro reset_ppu_addr
+        lda #$00
+        sta ppu_addr
+        sta ppu_addr
+    .macend
+
+    .macro reset_ppu_scroll
+        lda #0
+        sta ppu_scroll
+        sta ppu_scroll
+    .macend
+
+    .macro set_ppu_addr
+        lda #>_1
+        sta ppu_addr
+        lda #<_1
+        sta ppu_addr
+    .macend
+
+    .macro set_ppu_addr_via_x
+        ldx #>_1
+        stx ppu_addr
+        ldx #<_1
+        stx ppu_addr
+    .macend
+
+    .macro set_ppu_scroll
+        lda #_1
+        sta ppu_scroll
+        lda #_2
+        sta ppu_scroll
+    .macend
+
     .macro sub_imm
         ; subtract immediate without carry
         sec
         sbc #_1
+    .macend
+
+    .macro write_ppu_data
+        lda #_1
+        sta ppu_data
     .macend

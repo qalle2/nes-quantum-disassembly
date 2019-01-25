843780453a06421d52cf91e80e70d932656edff9
Kalle <qalle85@gmail.com>
2019-01-25 05:49:30 +0200

    check that files exist

--- assemble.bat
+++ assemble.bat
@@ -1,12 +1,23 @@
 @echo off
 cls
 
+if exist chr.bin goto assemble
+echo Error: chr.bin not found, cannot assemble. See the readme file.
+goto end
+
+:assemble
 echo *** Assembling iNES file ***
 ophis -v -o quantum.nes quantum.asm
 if errorlevel 1 goto end
 if not exist quantum.nes goto end
 echo.
 
+if exist quantum-original.nes goto verify
+echo Warning: quantum-original.nes not found, cannot verify the assembled file.
+echo See the readme file.
+goto end
+
+:verify
 echo *** Verifying iNES file ***
 fc /b quantum-original.nes quantum.nes
 
